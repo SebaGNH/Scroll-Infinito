65a7029b54ece5ff916955cd057dd898b3a72051
Sebastián González <seba.gnh@hgmail.com>
2022-01-07 15:27:44 -0300

    Se cargan las siguientes páginas correctamente al llegar al final

--- js/app.js
+++ js/app.js
@@ -1,28 +1,51 @@
 'use strict'
+let pagina = 1;
+//Creamos el observador
+let observador = new IntersectionObserver((entradas, observador)=>{
+    //console.log(entradas);
+    entradas.forEach(entrada =>{
+        if(entrada.isIntersecting){
+            pagina ++;
+            cargarProyecto();
+        }
+    });
+},{
+    rootMargin: '0px 0px 0px 0px',
+    threshold: 1.0
+});
 
+
+
+let peliculas = "";
 const cargarProyecto = async () =>{
 
     try {
-        const apiKey = "https://api.themoviedb.org/3/movie/popular?api_key=094387692d60e64212bac949a60c19bc";
+        const apiKey = `https://api.themoviedb.org/3/movie/popular?api_key=094387692d60e64212bac949a60c19bc&languaje=es-AR&page=${pagina}`;
         const respuesta = await fetch(apiKey);
         //console.log(respuesta);
 
         const datos = await respuesta.json();
-        console.log(datos.results[0]);
+        //console.log(datos.results[0]);
 
-        let peliculas = "";
+        
         datos.results.forEach(pelicula => {
             //console.log(pelicula.title);
-            const imgPortada = `https://image.tmdb.org/t/p/w500/${pelicula.backdrop_path}`;
+            const imgPortada = `https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`;
             
             peliculas += `
             <div class="pelicula">
-                <img class="portada" src="${imgPortada}" alt="">
-                <h2 class="titulo">${pelicula.original_title}</h2>
+                <img class="imgPortada" src="${imgPortada}" alt="">
+                <h2 class="tituloOriginal">${pelicula.original_title}</h2>
             </div>
             `;
         });
         document.getElementById("contenedor").innerHTML = peliculas;
+
+        const peliculasEnPantalla = document.querySelectorAll('.contenedor .pelicula');
+        //console.log(peliculasEnPantalla);
+        let ultimaPelicula = peliculasEnPantalla[peliculasEnPantalla.length -1];
+        //console.log(ultimaPelicula);
+        observador.observe(ultimaPelicula);
         
     } catch (error) {
         console.log(error);
